b0f33af206593407c79f18570708f116e1ef30c4
sumithababu <57611642+sumithababu@users.noreply.github.com>
2019-11-15 10:37:30 +0530

    update

--- dice sum 2/src/Sum/Friendsinterest.java
+++ dice sum 2/src/Sum/Friendsinterest.java
@@ -4,26 +4,26 @@ public class Friendsinterest
 {
 	public static void main(String[] args)
 	{
-	Scanner sc1 = new Scanner(System.in);
+	
 	int rate,threshold;
-	double interest;
+	
 	double deposit;
+	Scanner sc1 = new Scanner(System.in);
+	System.out.println("enter the deposit value");
+	deposit = sc1.nextDouble();
 
-	System.out.println( "enter the rate value");
+   System.out.println( "enter the rate value");
 	rate = sc1.nextInt();
 	
 	System.out.println("enter the threshold value");
 	threshold = sc1.nextInt();
-
-	System.out.println("enter the deposit value");
-	deposit = sc1.nextDouble();
-	 int year = 0;
-	interest = 0;
+	
+	int year=0;
 	while(deposit<threshold)
 	{
-	interest=deposit*(rate/100);
+	double interest=((deposit/100)*rate);
 	   deposit=deposit+interest;
-	   year++;
+	   year++;                                                                                  
 	}
 	   System.out.println("the number of years is"+ year);
 	
